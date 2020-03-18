f9fb55c941539e439167e79acf148c2e4affb73e
HarinHaoWu <master.harin@gmail.com>
2020-03-18 10:12:51 -0700

    Set Up Firebase

---
+++ Anna/DailyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DailyViewController.swift
+//  Anna
+//
+//  Created by Harin Wu on 2020-03-18.
+//  Copyright © 2020 Hao Wu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class DailyViewController: UIViewController {
+    @IBOutlet weak var Date: UILabel!
+    @IBOutlet weak var DailyPic: UIImageView!
+    @IBOutlet weak var DailyMessage: UITextView!
+    @IBOutlet weak var DailyLink: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
---
+++ Anna/SecretViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecretViewController.swift
+//  Anna
+//
+//  Created by Harin Wu on 2020-03-18.
+//  Copyright © 2020 Hao Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecretViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
